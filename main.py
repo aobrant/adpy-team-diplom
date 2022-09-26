3927a03a02d369c6217ca19511a682c0996dc832
password <moto198720@yandex.ru>
2022-09-26 14:32:11 +0300

    Код для БД

--- main.py
+++ main.py
@@ -25,22 +25,41 @@ session = Session()
 #
 # for c in session.query(Stranger).all():
 #     print(c)
+if __name__ == '__main__':
 
-with open('fixtures/tests_data.json', 'r', encoding='utf-8') as fd:
-    data = json.load(fd)
+    with open('fixtures/tests_data 2.json', 'r', encoding='utf-8') as fd:
+        data = json.load(fd)
 
-for record in data:
-    model = {
-        'user_stranger': User_stranger,
-        'user': User,
-        'stranger': Stranger,
-    }[record.get('model')]
-    session.add(model(id=record.get('pk'), **record.get('fields')))
+    for record in data:
+        model = {
+            'user_stranger': User_stranger,
+            'user': User,
+            'stranger': Stranger,
+        }[record.get('model')]
+        session.add(model(id=record.get('pk'), **record.get('fields')))
 
-session.commit()
+    session.commit()
 
+    for c in session.query(User).all():
+        print(c)
 
-for c in session.query(User).all():
-    print(c)
+age = input("Введите желаемый возраст партнера: ")
+# gender = input("Введите желаемый пол партнера: ")
+# city = input("Введите город партнера: ")
+
+# for i in session.query(Stranger).join(User_stranger, Stranger.id == User_stranger.stranger_id).\
+#         join(User, User.id == User_stranger.user_id).\
+#         filter(User.city.like(f"{city}")).all():
+#     print(f"Партнер {i} подходит по Вашим параметрам")
+#
+# for i in session.query(Stranger).join(User_stranger, Stranger.id == User_stranger.stranger_id).\
+#         join(User, User.id == User_stranger.user_id).\
+#         filter(User.gender.like(f"{gender}")).all():
+#     print(f"Партнер {i} подходит по Вашим параметрам")
+
+for i in session.query(Stranger).join(User_stranger, Stranger.id == User_stranger.stranger_id).\
+        join(User, User.id == User_stranger.user_id).\
+        filter(User.age.like(f"{age}")).all():
+    print(f"Партнер {i} подходит по Вашим параметрам")
 
 session.close()
